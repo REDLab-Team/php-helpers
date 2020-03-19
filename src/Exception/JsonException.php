14578c1bf55ef6d348575fb122885e98609896f3
Jean-Baptiste Motto <contact@hello-motto.fr>
2020-03-19 12:04:25 +0100

    Change Errors Management

--- src/Exception/JsonException.php
+++ src/Exception/JsonException.php
@@ -12,37 +12,13 @@ use RedlabTeam\Helper\Arr;
 class JsonException extends \Exception
 {
     /**
-     * @var int
-     */
-    const UNKNOWN_ERROR = 899;
-
-    /**
      * JsonException constructor.
+     *
+     * @param string $message
      * @param int $code
      */
-    public function __construct(int $code = 0, $message = null)
+    public function __construct(string $message = '', int $code = 0)
     {
-        $message = $message ?? self::getErrorMessage($code);
-
         parent::__construct($message, $code);
-    }
-
-    /**
-     * @param int $code
-     * @return string
-     */
-    public static function getErrorMessage(int $code): string
-    {
-        $errors = [
-            JSON_ERROR_NONE => 'No JSON error',
-            JSON_ERROR_DEPTH => 'JSON Maximum stack depth exceeded',
-            JSON_ERROR_STATE_MISMATCH => 'State mismatch (invalid or malformed JSON)',
-            JSON_ERROR_CTRL_CHAR => 'JSON Control character error, possibly incorrectly encoded',
-            JSON_ERROR_SYNTAX => 'JSON Syntax error',
-            JSON_ERROR_UTF8 => 'JSON Malformed UTF-8 characters, possibly incorrectly encoded',
-            self::UNKNOWN_ERROR => 'Unknown JSON error'
-        ];
-
-        return Arr::value($code, $errors, self::UNKNOWN_ERROR);
     }
 }
